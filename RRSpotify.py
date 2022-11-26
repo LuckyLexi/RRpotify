32d22c3bdf9cd3da5c515ab3f3a6ea7be05b463b
Lexi <105751836+LuckyLexi@users.noreply.github.com>
2022-11-26 21:47:57 +0000

    comments, changes to when it updates the bio

--- RRSpotify.py
+++ RRSpotify.py
@@ -1,15 +1,15 @@
-from recnetlogin import RecNetLogin
-import requests
-from twisted.internet import task, reactor
-import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import configparser
-import os
+from recnetlogin import RecNetLogin
 from dotenv import load_dotenv
+import configparser
+import sched, time
+import requests
+import spotipy
 import sys
+import os
 
-timeout = 45.0
 
+# ill be honest i dont remeber what this does all i know if i delete it it doesnt work
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -18,10 +18,13 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+#reading the config file, loading the env
 config = configparser.ConfigParser()
 config.read("config.ini")
 load_dotenv(resource_path('.env'))
+sc = sched.scheduler(time.time, time.sleep)
 
+# checking if theres a config file, making one if theres not
 try: 
     print(config['DONTCHANGE']['config_check'])
 except:
@@ -31,8 +34,7 @@ except:
 
     config.add_section('DONTCHANGE')
     config['DONTCHANGE']['config_check'] = 'Config file found.'
-    config['DONTCHANGE']['song_compare'] = 'Placeholder'
-
+    config['DONTCHANGE']['song_compare'] = 'placeholder'
     config.add_section('BIO')
     config['BIO']['savebio'] = 'false'
     config['BIO']['bio'] = ''
@@ -41,11 +43,11 @@ except:
     config['LOGIN']['storelogin'] = 'false'
     config['LOGIN']['YOURUSERNAME'] = ''
     config['LOGIN']['YOURPASSWORD'] = ''
-
-    with open("config.ini", 'w') as f:
+    with open("config.ini", 'w') as f: 
         config.write(f)
 
 
+#getting users username/password
 if config.getboolean('LOGIN', 'storelogin') == True:
     YOURUSERNAME = config['LOGIN']['YOURUSERNAME']
     YOURPASSWORD = config['LOGIN']['YOURPASSWORD']
@@ -91,11 +93,13 @@ else:
         with open("config.ini", 'w') as configfile:
             config.write(configfile)
         
+#setting spotipy auth stuff
 client_id = os.environ['client_id']
 client_secret = os.environ['client_secret']
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-currently-playing'
 
+#asking user if theyd like stuff added to their bio
 bio = ''
 bioconfirm = 'n'
 def bioconfig(bioconfirm):
@@ -124,9 +128,9 @@ def bioconfig(bioconfirm):
         config.write(configfile)
 
     return bio
-
 config.read("config.ini")
 
+#getting saved bio if its there
 if config.getboolean('BIO', 'savebio') == False:
     bio = bioconfig(bioconfirm)
 elif config.getboolean('BIO', 'savebio') == True: 
@@ -143,56 +147,90 @@ elif config.getboolean('BIO', 'savebio') == True:
     else: 
         bio = config['BIO']['bio']
 
+#recroom login 
 def login(YOURUSERNAME, YOURPASSWORD):
   rnl = RecNetLogin(username=YOURUSERNAME, password=YOURPASSWORD)
   token = rnl.get_token(include_bearer=True)
   return token
 token = login(YOURUSERNAME, YOURPASSWORD)
 
-def get_current_song(scope, client_id, client_secret, redirect_uri):
 
+def song_info(scope, client_id, client_secret, redirect_uri):
+    
+    #authorizationing to spotify with spotipy
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         scope=scope,
         client_id=client_id,
         client_secret=client_secret,
         redirect_uri=redirect_uri)
     )
-    
+
+    #currently playing song request
     spotify_resp = sp.current_user_playing_track()
 
+    #check to see if song is being played and handling for if not
     if spotify_resp == None or spotify_resp['is_playing'] == False:
         song = 'Not Playing Any Songs Currently'
-        return song
-    try:
-        track_name = spotify_resp['item']['name']
-        artists = [artist for artist in spotify_resp['item']['artists']]
-        artist_names = ', '.join([artist['name'] for artist in artists])
-        song = f'{track_name} by {artist_names}!'
-    except None or TypeError:
-        song =  'Nothing Right Now!'
-    return song
+        return song, 45.0
+
+    #formating the song name 
+    track_name = spotify_resp['item']['name']
+    artists = [artist for artist in spotify_resp['item']['artists']]
+    artist_names = ', '.join([artist['name'] for artist in artists])
+    song = f'{track_name} by {artist_names}!'
+
+    #getting the remaining playtime on the song 
+    song_timestamp = spotify_resp['progress_ms']
+    song_duration = spotify_resp['item']['duration_ms']
+    remaining_playtime_ms = song_duration-song_timestamp-13
+    remaining_playtime = round(remaining_playtime_ms / 1000, 2)
+    if remaining_playtime >= 70.0:
+        remaining_playtime = 45.0
+    
+    #returning variables that the bio update needs
+    return song, remaining_playtime
 
 def rr_bio_change(token, bio):
-    song = get_current_song(scope, client_id, client_secret, redirect_uri)
+
+    #calling song_info fuction
+    song_info_ = song_info(scope, client_id, client_secret, redirect_uri)
+    song = song_info_[0] #song
+    remaining_playtime = song_info_[1] #remaining_playtime
 
     if song != config['DONTCHANGE']['song_compare']:
-
+        
+        #writing the song to the config file
         config['DONTCHANGE']['song_compare'] = song
         with open("config.ini", 'w') as configfile:
             config.write(configfile)
-
-        rec_resp = requests.put(f'https://accounts.rec.net/account/me/bio', headers= {"Authorization": token}, 
-        data = {'bio':f"Listening To:\n{song}\n{bio}"})
+        
+        #request for rec room bio change
+        rec_resp = requests.put(f'https://accounts.rec.net/account/me/bio', 
+            headers= {"Authorization": token}, 
+            data = {'bio':f"Listening To:\n{song}\n{bio}"}
+        )
+        
+        #renew token if the request returning 401
         if rec_resp.status_code == 401:
             token = login(YOURUSERNAME, YOURPASSWORD)
             rec_resp = requests.put(f'https://accounts.rec.net/account/me/bio', headers= {"Authorization": token}, 
             data = {'bio':f"Listening To:\n{song}\n{bio}"})
 
-        print(f'Currently playing song: {song}')
-        print('Bio Change Success!')
-    pass
+        
+        #just showing some info to give some feedback to the user 
+        if rec_resp.status_code == 200:
+            print(f'Currently playing song: {song}')
+            print('Bio change succes!')
+        else: 
+            print('-----')
+            print(f'Currently playing song: {song}')
+            print('Bio change failed.')
+            print(f'Error code:{rec_resp.status_code}')
+            print('-----')
 
-l = task.LoopingCall(rr_bio_change, token, bio)
-l.start(timeout)
+        #starting loop again after the song ends
+    sc.enter(remaining_playtime, 1, rr_bio_change, (token, bio))
 
-reactor.run()
+#start the loop 
+sc.enter(1.0, 1, rr_bio_change, (token, bio))
+sc.run()
